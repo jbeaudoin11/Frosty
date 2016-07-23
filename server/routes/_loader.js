bb8b8b095e6e23edfe7b5c174cbaee2556f77a65
Ubuntu <ubuntu@ip-172-31-40-1.us-west-2.compute.internal>
2016-07-23 01:43:41 +0000

    Changer les callback par des promises pour _loader des routes

--- server/routes/_loader.js
+++ server/routes/_loader.js
@@ -1,6 +1,8 @@
 "use strict";
 //load all routes in files
 
+var Promise = require("promise");
+
 var path = require("path");
 var logger = require("winston");
 var colors = require("colors");
@@ -18,80 +20,84 @@ module.exports = class RoutesLoader {
 		]
 		
 		ScriptLoader(__dirname, excludeFiles, (filename) => {
-			var data = require(`./${filename}`);
+			var _routes = require(`./${filename}`);
 			
-			//test if the data is an array (maybe not the best way)
-			if(typeof(data.forEach) == "function"){
-				data.forEach((r) => {
-					this._testRouteObject(r, (err) => {
-						if(err){
-							logger.error(err.error.red);
-						} else {
-							this.router[path.basename(filename, ".js")](r.url, r.listeners);
-						}
+			//test if _routes is an array (maybe not the best way)
+			if(typeof(_routes.forEach) == "function"){
+				_routes.forEach((route, index) => {
+
+					//Test each route obj
+					this._testRouteObject(route, index)
+					.then(() => {
+						this.router[path.basename(filename, ".js")](r.url, r.listeners);
 					})
+					.catch(this.loadingErrorHandler);
+
 				})
+			} else {
+				//TODO trhow error
 			}
 		})
 	}
 
-	_testRouteObject(route, callback) {
+	loadingErrorHandler(err) {
+		//Handle loading errors
+		logger.error(err.error.red);
+	}
+
+	_testRouteObject(route, index) {
 		//Execute all tests on the route object
 
-		this._testRouteObjectUrl(route, (err) => {
-			if(err){
-				callback(err)
-			} else {
-				this._testRouteObjectListeners(route, (err) => {
-					if(err){
-						callback(err)
-					} else {
-						this._testRouteObjectListenersItems(route, callback);
-					}
-				})
-			}
-		})
+		return (
+			this._testRouteObjectUrl(route, index)
+				.then(this._testRouteObjectListeners.bind(this, route, index))
+				.then(this._testRouteObjectListenersItems.bind(this, route, index))
+		);
 	}
 
-	_testRouteObjectUrl(r, callback) {
+	_testRouteObjectUrl(r, index) {
 		//Test if the url is defined
-
-		if(r.url != undefined){
-			callback();
-		} else {
-			callback({
-				"error" : "ROUTE, NO URL",
-			});
-		}
+		return new Promise((res, rej) => {
+			if(r.url != undefined){
+				res();
+			} else {
+				rej({
+					"error" : `ROUTE [${index}] HAS NO URL`,
+				});
+			}
+		});
 	}
 
-	_testRouteObjectListeners(r, callback) {
+	_testRouteObjectListeners(r, index) {
 		//Test if listeners is undefined and have at least 1 listener
-
-		if(r.listeners != undefined && r.listeners.length > 0){
-			callback();
-		} else {
-			callback({
-				"error" : `ROUTE [${r.url}], LISTENERS MUST BE AN ARRAY WITH AT LEAST 1 LISTENER`,
-			});
-		}
+		return new Promise((res, rej) => {
+			if(r.listeners != undefined && r.listeners.length > 0){
+				res();
+			} else {
+				rej({
+					"error" : `ROUTE [${index} | ${r.url}], LISTENERS MUST BE AN ARRAY WITH AT LEAST 1 LISTENER`,
+				});
+			}
+		});
 	}
 
-	_testRouteObjectListenersItems(r, callback) {
+	_testRouteObjectListenersItems(r, index) {
 		//Test if a listener in the listeners is undefined
 
 		var i;
-		var result = r.listeners.every((listener, index) => {
-			i = index;
+		var result = r.listeners.every((listener, _index) => {
+			i = _index;
 			return Boolean(listener) || false;
 		})
 
-		if(result){
-			callback()
-		} else {
-			callback({
-				"error" : `ROUTE [${r.url}], LISTENER [${i}] IS UNDEFINED`,
-			});
-		}
+		return new Promise((res, rej) => {
+			if(result){
+				res();
+			} else {
+				rej({
+					"error" : `ROUTE [${index} | ${r.url}], LISTENER [${i}] IS UNDEFINED`,
+				});
+			}
+		});
 	}
 }
